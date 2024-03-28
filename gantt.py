cc70c807bbfd06bd32042a1851ddca939f04a436
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-27 22:47:34 -0400

    Initialize project file

---
+++ gantt.py
@@ -0,0 +1,20 @@
+##################################################################'''
+# Nicholas Rios - 0perating Systems Gantt Chart Project
+'''
+Requirements:
+1. Generate 10 Processes
+2. id, random number burst, arrival time (0...11)
+
+Functions:
+1. FCFS
+2. SRTF
+3. Round Robin (q=2)
+
+Calculate:
+1. Average wait time
+2. Average turnaround time
+'''
+###############################################################################'''
+
+def fcfs_gantt():
+    pass
