d570fb6a65b0cf585ac691ba82021b19bb7d5393
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-27 23:01:38 -0400

    Create process class and function to generate processes

--- gantt.py
+++ gantt.py
@@ -14,7 +14,23 @@ Calculate:
 1. Average wait time
 2. Average turnaround time
 '''
-###############################################################################'''
+##############################################################################
+import random
 
-def fcfs_gantt():
+class Process:
+    def __init__(self, id, arrival_time):
+        self.id = id
+        self.burst = random.randint(1, 30)
+        self.arrival_time = arrival_time
+
+
+# Generates 10 processes with arrival times between 0 and 11
+def generate_processes():
+    processes = []
+    for i in range(10):
+        processes.append(Process(i + 1, random.randint(0,11)))
+    return processes
+
+
+def fcfs_gantt(processes):
     pass
