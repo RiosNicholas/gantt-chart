d85bc6be3b39419586cb2e98f484256a1a96ee6e
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-31 21:47:19 -0400

    Remove comments

--- gantt.py
+++ gantt.py
@@ -1,20 +1,3 @@
-##################################################################'''
-# Nicholas Rios - 0perating Systems Gantt Chart Project
-#
-#Requirements:
-# 1. Generate 10 Processes
-# 2. id, random number burst, arrival time (0...11)
-#
-# Functions:
-# 1. FCFS
-# 2. SRTF
-# 3. Round Robin (q=2)
-
-# Calculate:
-# 1. Average wait time
-# 2. Average turnaround time
-#
-##############################################################################
 import random
 
 class Process:
